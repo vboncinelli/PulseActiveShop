db0bc60c2bc55931bf8bec9134ab9d9a1217b93c
Vanni Boncinelli <vanni.boncinelli@remira.com>
2024-01-16 09:57:50 +0100

    fix

--- Api/PulseActiveShop.Api.Entities/OrderItem.cs
+++ Api/PulseActiveShop.Api.Entities/OrderItem.cs
@@ -2,6 +2,8 @@
 {
     public class OrderItem : BaseApiEntity
     {
+        public int OrderId { get; set; }
+
         public ProductOrdered ItemOrdered { get; set; } = null!;
 
         public decimal UnitPrice { get; set; }
